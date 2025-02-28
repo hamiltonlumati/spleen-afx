d5cb1a9b032bde8f7d32f2aa18dcf0e0e51fac6d
user.email <hamiltonlumati@gmail.com>
2025-02-28 06:00:12 +0100

    update

--- app/server.ts
+++ app/server.ts
@@ -1,12 +1,32 @@
 import express from 'express';
+import AppDataSource from 'config/database';
+import PlaylistRoutes from  './routes/Playlist';
+import UserRoutes from './routes/User';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, TypeScript with Express!');
-});
+const startServer = async() =>{
+    AppDataSource.initialize()
+        .then(() => {
+            console.log("Data Source has been initialized!")
+        })
+        .catch((err) => {
+            console.error("Error during Data Source initialization", err)
+        })
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+    app.get('/', (req, res) => {
+        res.send('Hello, TypeScript with Express!');
+    });
+    
+    app.use(express.json());
+
+    app.use('/api', PlaylistRoutes);
+    app.use('/api',UserRoutes);
+
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+}
+
+startServer();
